d6633cff9840860eb7446cb60957423327667124
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-08-26 16:52:49 -0400

    add modal

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Modal from '../Modal';
 
 
 function PhotoList({category}) {
@@ -101,21 +102,30 @@ function PhotoList({category}) {
     ]);
 
     const currentPhotos = photos.filter((photo) => photo.category === category)
+    const [currentPhoto, setCurrentPhoto] = useState();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const toggleModal = (image, i) => {
+      setCurrentPhoto({...image, index: i})
+      setIsModalOpen(true);
+    }
 
     return (
     <div>
         <div className="flex-row">
-            {currentPhotos.map((image, i) => (
-                <img
-                src={require(`../../assets/small/${category}/${i}.jpg`).default}
-                alt={image.name}
-                className="img-thumbnail mx-1"
-                key={image.name}
-              />
-            ))}
+          {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+          {currentPhotos.map((image, i) => (
+            <img
+              src={require(`../../assets/small/${category}/${i}.jpg`).default}
+              alt={image.name}
+              className="img-thumbnail mx-1"
+              key={image.name}
+              onClick={() => toggleModal(image, i)}
+            />
+          ))}
         </div>
     </div>
-  )
+    )
 }
 
 export default PhotoList;
