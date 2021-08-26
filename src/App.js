fba698b1ec1cdf304bde5f6f72264122718b9af0
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-08-26 15:39:01 -0400

    added contact form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import './App.css';
 import About from './components/About';
+import ContactForm from './components/Contact';
 import Gallery from './components/Gallery';
 import Nav from './components/Nav'
 
@@ -25,6 +26,7 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery
           currentCategory={currentCategory}
         ></Gallery>
